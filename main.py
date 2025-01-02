7018129326db9dd0c72793414a1680d4de9065ca
yusufemir1 <yusufemirgundogdu70@gmail.com>
2025-01-02 19:26:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,23 @@ bot = commands.Bot(command_prefix='$', intents=intents)
 @bot.event
 async def on_ready():
     print(f'{bot.user} olarak giriş yaptık')
-
+!bunu ekledım
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+    bunu ekledim
+    @bot.command()
+async def repeat(ctx, times: int, content: str):
+    """Repeats a message multiple times."""
+    for i in range(times):
+        await ctx.send(content)
+        bunu ekledim
+        @bot.command()
+async def repeat(ctx, times: int, content: str):
+    """Repeats a message multiple times."""
+    for i in range(times):
+        await ctx.send(content)     
 @bot.command()
 async def hello(ctx):
     await ctx.send(f'Merhaba! Ben {bot.user}, bir Discord sohbet botuyum!')
